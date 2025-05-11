85b95170020a45eaea56e26ba1ef44740ed9f199
Coque Couto <coque.couto@gmail.com>
2025-05-11 21:02:26 +0200

    First complete version

--- ipv4.el
+++ ipv4.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025 Coque Couto
 
 ;; Author: Coque Couto <coque.couto at gmail.com>
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs))
 ;; Keywords: comm lisp tools
 ;; URL: https://github.com/coquec/ipv4
@@ -33,14 +33,11 @@
 
 ;;;###autoload
 (defun ipv4-string-to-int (ip)
-  "Convert an IPv4 address in string format to an integer.
+  "Convert the IPv4 address in the string IP to an integer.
 
 Return NIL if IP is an invalid IPv4 address."
-  (let ((parts (mapcar #'string-to-number (split-string ip "\\." t))))
-    (when (and (ipv4--byte-p (nth 0 parts))
-               (ipv4--byte-p (nth 1 parts))
-               (ipv4--byte-p (nth 2 parts))
-               (ipv4--byte-p (nth 3 parts)))
+  (when (ipv4-valid ip)
+    (let ((parts (mapcar #'string-to-number (split-string ip "\\." t))))
       (+ (ash (nth 0 parts) 24)
          (ash (nth 1 parts) 16)
          (ash (nth 2 parts) 8)
@@ -48,7 +45,7 @@ Return NIL if IP is an invalid IPv4 address."
 
 ;;;###autoload
 (defun ipv4-int-to-string (ip)
-  "Convert an integer to an IPv4 address string."
+  "Convert an integer IP to an IPv4 address string."
   (format "%d.%d.%d.%d"
           (logand (ash ip -24) #xFF)
           (logand (ash ip -16) #xFF)
@@ -56,19 +53,23 @@ Return NIL if IP is an invalid IPv4 address."
           (logand ip #xFF)))
 
 ;;;###autoload
-(defun ipv4-valid-p (ip)
-  "Return t if the string IP is a valid IPv4 address."
+(defun ipv4-valid (ip)
+  "Return the string IP if it is a valid IPv4 address.
+
+Return nil otherwise."
   (when (string-match
-         "^\\([1-9]?[0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]\\)\\(\\.\\([1-9]?[0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]\\)\\)\\{3\\}$"
+         "^\\([1-9]?[0-9]\\|1[0-9][0-9]\\|2[0-4][0-9]\\|25[0-5]\\)\\(\\.\\([1-9]?[0-9]\\|1[0-9][0-9]\\|2[0-4][0-9]\\|25[0-5]\\)\\)\\{3\\}$"
          ip)
-    t))
+    ip))
 
 
 ;; CIDR functions.
 
 ;;;###autoload
 (defun ipv4-cidr-to-cons (cidr)
-  "Convert a CIDR network address \"aaa.bbb.ccc.ddd/xx\" to a
+  "Extract network address and mask from a string in CIDR format.
+
+Convert a string with a CIDR network address \"aaa.bbb.ccc.ddd/xx\" to a
 cons with the IP address in the first element and the length of the
 network mask in the second element.
 
@@ -77,42 +78,146 @@ Return NIL if CIDR has an invalid format."
     (when (= 2 (length parts))
       (let ((ip (ipv4-string-to-int (car parts)))
             (mask (string-to-number (cadr parts))))
-        (when (and ip (ipv4-integer-mask-p mask))
+        (when (and ip (ipv4-cidr-mask-p mask))
           (cons ip mask))))))
 
 ;;;###autoload
 (defun ipv4-cons-to-cidr (cons)
-  "Convert a CONS with an IP address in the first element and a network
-mask in the second one, both as integers, to a string with format
+  "Produce a string in CIDR format from a cons with a network and a mask.
+
+Convert a CONS with an IP address in the first element and a network
+mask length in the second one, both as integers, to a string with format
 \"aaa.bbb.ccc.ddd/xx\".
 
 Return NIL if the IPv4 address or the mask are invalid."
   (when (consp cons)
     (let ((ip (car cons))
           (mask (cdr cons)))
-      (when (and (integerp ip)
-                 (ipv4-integer-mask-p mask))
+      (when (and (integerp ip) (ipv4-cidr-mask-p mask))
         (concat (ipv4-int-to-string ip) "/" (number-to-string mask))))))
+
+;;;###autoload
+(defun ipv4-network (cons)
+  "Return the network address in CONS as an integer.
+
+Return nil for network masks of 31 or 32 bits.  For 31 bits, the two
+available addresses are for hosts, according to RFC-3021.  A 32 bit mask
+denotes a host.
+
+The expected values in CONS are described in `ipv4-cidr-to-cons'."
+  (when (< (cdr cons) 31)
+    (ipv4-raw-network cons)))
+
+;;;###autoload
+(defun ipv4-raw-network (cons)
+  "Return the network address in CONS as an integer.
+
+The expected values in CONS are described in `ipv4-cidr-to-cons'.
+
+Do not take any special action for network masks of 31 or 32 bits.
+
+The returned mask can be converted to a string such as \"255.255.255.0\"
+with `ipv4-int-to-string'."
+  (logand (car cons) (ipv4-cidr-to-mask (cdr cons))))
+
+;;;###autoload
+(defun ipv4-broadcast (cons)
+  "Return the network broadcast address in CONS as an integer.
+
+The expected values in CONS are described in `ipv4-cidr-to-cons'.
+
+Return nil for network masks of 31 or 32 bits.  For 31 bits, the two
+available addresses are for hosts, according to RFC-3021.  A 32 bit mask
+denotes a host.
+
+The returned mask can be converted to a string such as \"255.255.255.0\"
+with `ipv4-int-to-string'."
+  (when (< (cdr cons) 31)
+    (ipv4-raw-broadcast cons)))
+
+;;;###autoload
+(defun ipv4-raw-broadcast (cons)
+  "Return the network broadcast address in CONS as an integer.
+
+The expected values in CONS are described in `ipv4-cidr-to-cons'.
+
+Do not take any special action for network masks of 31 or 32 bits.
+
+The returned mask can be converted to a string such as \"255.255.255.0\"
+with `ipv4-int-to-string'."
+  (+ (ipv4-raw-network cons)
+     (logand #xFFFFFFFF (lognot (ipv4-cidr-to-mask (cdr cons))))))
+
+;;;###autoload
+(defun ipv4-host-count (cons)
+  "Return the number of hosts in the network in CONS.
+
+The expected values in CONS are described in `ipv4-cidr-to-cons'."
+  (cond ((= (cdr cons) 32)
+         1)
+        ((= (cdr cons) 31)
+         2)
+        (t
+         (- (expt 2 (- 32 (cdr cons))) 2))))
+
+;;;###autoload
+(defun ipv4-first-host (cons)
+  "Return the IP address of the first host in the network in CONS.
+
+The expected values in CONS are described in `ipv4-cidr-to-cons'."
+  (if (>= (cdr cons) 31)
+      (ipv4-raw-network cons)
+    (+ 1 (ipv4-raw-network cons))))
+
+;;;###autoload
+(defun ipv4-last-host (cons)
+  "Return the IP address of the last host in the network in CONS.
+
+The expected values in CONS are described in `ipv4-cidr-to-cons'."
+  (if (>= (cdr cons) 31)
+      (ipv4-raw-broadcast cons)
+    (- (ipv4-raw-broadcast cons) 1)))
 
 
 ;; Mask functions.
 
 ;;;###autoload
-(defun ipv4-integer-mask-p (mask)
-  "Return t if MASK is an integer between 0 and 32."
-  (when (integerp mask)
-    (and (>= mask 0) (<= mask 32))))
-
-
-;; Useful internal functions.
+(defun ipv4-cidr-mask-p (cidr)
+  "Return t if CIDR is an integer between 0 and 32."
+  (and (integerp cidr)
+       (>= cidr 0)
+       (<= cidr 32)))
 
 ;;;###autoload
-(defun ipv4--byte-p (n)
-  "Return t if N is an integer between 0 and 255."
-  (and (integerp n)
-       (>= n 0)
-       (<= n 255)))
+(defun ipv4-cidr-to-mask (cidr)
+  "Convert a mask length CIDR into a integer mask.
+
+Return nil if CIDR is not a valid mask length."
+  (when (ipv4-cidr-mask-p cidr)
+    (logand (ash -4294967296 (- cidr)) #xFFFFFFFF)))
+
+;;;###autoload
+(defun ipv4-mask-p (mask)
+  "Return t if MASK is a valid integer mask.
+
+An valid integer mask is a 32 bit integer with all of the 1s (if any) in
+its binary representation grouped to the left, with no 0s between them."
+  (and (integerp mask)
+       (or (= 0 mask)
+           (and (>= mask 0)
+                (<= mask #xFFFFFFFF)
+                (= #x80000000 (logand mask #x80000000) )
+                (= 2 (logcount (logxor mask (ash mask 1))))))))
+
+;;;###autoload
+(defun ipv4-cidr-to-wildcard (cidr)
+  "Return the wildcard corresponfig to a valid CIDR integer mask.
+
+Return nil if CIDR is not a valid mask length."
+  (let ((mask (ipv4-cidr-to-mask cidr)))
+    (when mask
+      (logand #xFFFFFFFF (lognot mask)))))
 
 (provide 'ipv4)
 
-;;; ipv4 ends here
+;;; ipv4.el ends here
--- test/test.el
+++ test/test.el
@@ -16,7 +16,7 @@
   (should-not (ipv4-string-to-int "0.0.256.0"))
   (should-not (ipv4-string-to-int "0.-1.0.0"))
   (should-not (ipv4-string-to-int "-1.0.0.0"))
-  (should-not (ipv4-string-to-int "-1.0.0.0/32")))
+  (should-not (ipv4-string-to-int "1.0.0.0/32")))
 
 (ert-deftest test-ipv4-valid-ints-to-strings ()
   (should (equal "0.0.0.0" (ipv4-int-to-string 0)))
@@ -55,12 +55,96 @@
   (should-not (ipv4-cons-to-cidr '(0 . 33))))
 
 
-;; Tests for mask functions.
-(ert-deftest test-ipv4-valid-integer-masks ()
-  (should (ipv4-integer-mask-p 0))
-  (should (ipv4-integer-mask-p 32)))
+;; Tests for network functions.
+(ert-deftest test-ipv4-network ()
+  (should (equal 0 (ipv4-network '(0 . 0))))
+  (should (equal #x0A000000 (ipv4-network '(#x0A5D5D5D . 8))))
+  (should (equal #xAC100000 (ipv4-network '(#xAC10FFFF . 12))))
+  (should (equal #xC0A80000 (ipv4-network '(#xC0A85D5D . 16))))
+  (should (equal nil (ipv4-network '(#xC0A80101 . 31))))
+  (should (equal nil (ipv4-network '(#xC0A80101 . 32)))))
 
-(ert-deftest test-ipv4-invalid-integer-masks ()
-  (should-not (ipv4-integer-mask-p nil))
-  (should-not (ipv4-integer-mask-p -1))
-  (should-not (ipv4-integer-mask-p 33)))
+(ert-deftest test-ipv4-broadcast ()
+  (should (equal #xFFFFFFFF (ipv4-broadcast '(0 . 0))))
+  (should (equal #x0AFFFFFF (ipv4-broadcast '(#x0A5D5D5D . 8))))
+  (should (equal #xAC1FFFFF (ipv4-broadcast '(#xAC105D5D . 12))))
+  (should (equal #xC0A8FFFF (ipv4-broadcast '(#xC0A85D5D . 16))))
+  (should (equal nil (ipv4-broadcast '(#xC0A80101 . 31))))
+  (should (equal nil (ipv4-broadcast '(#xC0A80101 . 32)))))
+
+(ert-deftest test-ipv4-host-count ()
+  (should (equal #xFFFFFFFE (ipv4-host-count '(0 . 0))))
+  (should (equal #xFFFFFE (ipv4-host-count '(#x0A5D5D5D . 8))))
+  (should (equal #xFFFFE (ipv4-host-count '(#xAC10FFFF . 12))))
+  (should (equal #xFFFE (ipv4-host-count '(#xC0A85D5D . 16))))
+  (should (equal 2 (ipv4-host-count '(#xC0A80101 . 31))))
+  (should (equal 1 (ipv4-host-count '(#xC0A80101 . 32)))))
+
+(ert-deftest test-ipv4-first-host ()
+  (should (equal 1 (ipv4-first-host '(0 . 0))))
+  (should (equal #x0A000001 (ipv4-first-host '(#x0A5D5D5D . 8))))
+  (should (equal #xAC100001 (ipv4-first-host '(#xAC10FFFF . 12))))
+  (should (equal #xC0A80001 (ipv4-first-host '(#xC0A85D5D . 16))))
+  (should (equal #xC0A80100 (ipv4-first-host '(#xC0A80101 . 31))))
+  (should (equal #xC0A80101 (ipv4-first-host '(#xC0A80101 . 32)))))
+
+(ert-deftest test-ipv4-last-host ()
+  (should (equal #xFFFFFFFE (ipv4-last-host '(0 . 0))))
+  (should (equal #x0AFFFFFE (ipv4-last-host '(#x0A5D5D5D . 8))))
+  (should (equal #xAC1FFFFE (ipv4-last-host '(#xAC10FFFF . 12))))
+  (should (equal #xC0A8FFFE (ipv4-last-host '(#xC0A85D5D . 16))))
+  (should (equal #xC0A80101 (ipv4-last-host '(#xC0A80101 . 31))))
+  (should (equal #xC0A80101 (ipv4-last-host '(#xC0A80101 . 32)))))
+
+
+;; Tests for mask functions.
+(ert-deftest test-ipv4-valid-cidr-masks ()
+  (should (ipv4-cidr-mask-p 0))
+  (should (ipv4-cidr-mask-p 32)))
+
+(ert-deftest test-ipv4-invalid-cidr-masks ()
+  (should-not (ipv4-cidr-mask-p nil))
+  (should-not (ipv4-cidr-mask-p -1))
+  (should-not (ipv4-cidr-mask-p 33)))
+
+(ert-deftest test-ipv4-valid-cird-to-mask ()
+  (should (equal 0 (ipv4-cidr-to-mask 0)))
+  (should (equal #x80000000 (ipv4-cidr-to-mask 1)))
+  (should (equal #xC0000000 (ipv4-cidr-to-mask 2)))
+  (should (equal #xFFFFFFFE (ipv4-cidr-to-mask 31)))
+  (should (equal #xFFFFFFFF (ipv4-cidr-to-mask 32))))
+
+(ert-deftest test-ipv4-invalid-cird-to-mask ()
+  (should-not (ipv4-cidr-to-mask -1))
+  (should-not (ipv4-cidr-to-mask 33))
+  (should-not (ipv4-cidr-to-mask "255.255.255.0")))
+
+(ert-deftest test-ipv4-valid-mask ()
+  (should (ipv4-mask-p 0))
+  (should (ipv4-mask-p #x80000000))
+  (should (ipv4-mask-p #xE0000000))
+  (should (ipv4-mask-p #xFE000000))
+  (should (ipv4-mask-p #xFFFF0000))
+  (should (ipv4-mask-p #xFFFFFFFE))
+  (should (ipv4-mask-p #xFFFFFFFF)))
+
+(ert-deftest test-ipv4-invalid-mask ()
+  (should-not (ipv4-mask-p -1))
+  (should-not (ipv4-mask-p 1))
+  (should-not (ipv4-mask-p #xFFFFEFFF))
+  (should-not (ipv4-mask-p #xFFFFEFFF))
+  (should-not (ipv4-mask-p #xFFFFCFFF))
+  (should-not (ipv4-mask-p #x1FFFFFFFF))
+  (should-not (ipv4-mask-p "255.255.255.0")))
+
+(ert-deftest test-ipv4-valid-wildcard ()
+  (should (equal #xFFFFFFFF (ipv4-cidr-to-wildcard 0)))
+  (should (equal #xFFFFFF (ipv4-cidr-to-wildcard 8)))
+  (should (equal #xFFFFF (ipv4-cidr-to-wildcard 12)))
+  (should (equal #xFFFF (ipv4-cidr-to-wildcard 16)))
+  (should (equal 1 (ipv4-cidr-to-wildcard 31)))
+  (should (equal 0 (ipv4-cidr-to-wildcard 32))))
+
+(ert-deftest test-ipv4-invalid-wildcard ()
+  (should-not (ipv4-cidr-to-wildcard -1))
+  (should-not (ipv4-cidr-to-wildcard 33)))
